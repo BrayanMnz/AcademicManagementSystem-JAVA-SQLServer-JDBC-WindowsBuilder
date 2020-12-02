2f0d8e28c286cd1fd02187ae137c292fd8bdf68d
Munoz <Munoz@MuozV>
2020-12-02 02:15:07 -0500

    Ver inscripciones

---
+++ FinalProject_BBDD/src/Visual/VerInscripciones.java
@@ -0,0 +1,136 @@
+package Visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class VerInscripciones extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			VerInscripciones dialog = new VerInscripciones();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public VerInscripciones() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowOpened(WindowEvent e) {
+				
+				cargarInscripciones();
+			}
+		});
+		setTitle("Inscripciones");
+		setBounds(100, 100, 565, 365);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JScrollPane scrollPane = new JScrollPane();
+			contentPanel.add(scrollPane, BorderLayout.CENTER);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+	
+private void cargarInscripciones() {
+		
+		Connection conn = null;
+	    try
+	    {
+	    	 String dbURL = "jdbc:sqlserver://MUÑOZV";
+	         String user = "Brayan";
+	         String pass = "12345";
+	         conn = DriverManager.getConnection(dbURL, user, pass);
+	         if (conn != null) {
+	             System.out.println("Conexion establecida ");
+	         			      }
+	         String query = "SELECT * FROM INSCRIPCION";
+	    PreparedStatement st =conn.prepareStatement(query);
+	    ResultSet rs = st.executeQuery();
+	    
+	    
+	        //Create new table model
+	        DefaultTableModel tableModel = new DefaultTableModel();
+
+	        //Retrieve meta data from ResultSet
+	        ResultSetMetaData metaData = rs.getMetaData();
+
+	        //Get number of columns from meta data
+	        int columnCount = metaData.getColumnCount();
+
+	        //Get all column names from meta data and add columns to table model
+	        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++){
+	            tableModel.addColumn(metaData.getColumnLabel(columnIndex));
+	        }
+
+	        //Create array of Objects with size of column count from meta data
+	        Object[] row = new Object[columnCount];
+
+	        //Scroll through result set
+	        while (rs.next()){
+	            //Get object from column with specific index of result set to array of objects
+	            for (int i = 0; i < columnCount; i++){
+	                row[i] = rs.getObject(i+1);
+	            }
+	            //Now add row to table model with that array of objects as an argument
+	            tableModel.addRow(row);
+	        }
+
+	        //Now add that table model to your table and you are done :D
+	        tblInscripciones.setModel(tableModel);
+	    
+	    conn.close();
+	    }
+	    catch(Exception ex)
+	    {
+	    JOptionPane.showMessageDialog(null, ex.toString());
+	    }
+	}
+	
+	
+	
+}
